79082ff9a5345c2547d20e79d0ba9a52ac9881ca
Euphorbix <lumirose@gmail.com>
2022-10-21 16:00:29 +0200

    first commit - standard API structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is about estimating median house value in california',
+    author='Luis Miguel Rodriguez',
+    license='MIT',
+)
---
+++ tests/test_client.py
@@ -0,0 +1,5 @@
+import requests
+req = requests.get('http://127.0.0.1:5000/')
+print(req.status_code)
+print(req.headers)
+print(req.content)
